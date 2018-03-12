1ec016409e937fb635989c7ac810eb0d6ab568e0
stv <stv@stanford.edu>
2018-03-12 16:13:32 -0700

    TEMP: Add logging for "Graph This"

--- openedx/stanford/lms/djangoapps/instructor/views/api.py
+++ openedx/stanford/lms/djangoapps/instructor/views/api.py
@@ -1,4 +1,5 @@
 import gzip
+import logging
 import re
 import StringIO
 import urllib2
@@ -34,6 +35,7 @@ EXTRA_INSTRUCTOR_POST_ENDPOINTS = {
     'get_student_responses',
     'graph_course_forums_usage',
 }
+LOG = logging.getLogger('stanford.celery.task')
 
 
 @require_POST
@@ -233,11 +235,16 @@ def graph_course_forums_usage(request, course_id):
                 url = settings.LMS_ROOT_URL + url
                 request = urllib2.Request(url)
                 request.add_header('Accept-encoding', 'gzip')
-                url_handle = urllib2.urlopen(request)
-                if url_handle.info().get('Content-Encoding') == 'gzip':
-                    file_buffer = StringIO.StringIO(url_handle.read())
-                    url_handle = gzip.GzipFile(fileobj=file_buffer)
-                graph = generate_course_forums_d3(url_handle)
+                LOG.error("URL IS: %s", url)
+                LOG.error("REQUEST IS: %s", request)
+                try:
+                    url_handle = urllib2.urlopen(request)
+                    if url_handle.info().get('Content-Encoding') == 'gzip':
+                        file_buffer = StringIO.StringIO(url_handle.read())
+                        url_handle = gzip.GzipFile(fileobj=file_buffer)
+                    graph = generate_course_forums_d3(url_handle)
+                except Exception as e:
+                    LOG.error("Exception: %s", e)
                 break
     if graph:
         response = JsonResponse({
